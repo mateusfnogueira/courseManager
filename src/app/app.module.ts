13c6a5e5ede5d796e3a24baf16ba5cee12e20a31
Mateus <mateusfnogueira@outlook.com>
2020-11-15 19:31:31 -0300

    criação da tabela de cursos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CourseListComponent } from './courses/course-list.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CourseListComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent, CourseListComponent],
 })
 export class AppModule {}
---
+++ src/app/courses/course-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Course } from './course';
+
+@Component({
+  selector: 'app-course-list',
+  templateUrl: './course-list.component.html',
+})
+export class CourseListComponent implements OnInit {
+  courses: Course[] = [];
+
+  ngOnInit(): void {
+    this.courses = [
+      {
+        id: 1,
+        name: 'Angular: Forms',
+        imageUrl: '',
+        price: 99.99,
+        code: 'XPs-8796',
+        duration: 120,
+        rating: 5.4,
+        releaseDate: 'december, 2, 2019',
+      },
+    ];
+  }
+}
